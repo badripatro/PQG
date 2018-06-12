7a01b276638114d3763aa51895f991971cdc95e6
Badri Narayana Patro <patrobadri.iitb@gmail.com>
2018-06-12 18:58:53 +0530

    Add files via upload

---
+++ coco-caption/pycocoevalcap/eval.py
@@ -0,0 +1,102 @@
+__author__ = 'tylin'
+from tokenizer.ptbtokenizer import PTBTokenizer
+from bleu.bleu import Bleu
+from meteor.meteor import Meteor
+from rouge.rouge import Rouge
+from cider.cider import Cider
+
+class COCOEvalCap:
+    def __init__(self, coco, cocoRes):
+        self.evalImgs = []
+        self.eval = {}
+        self.imgToEval = {}
+        self.coco = coco
+        self.cocoRes = cocoRes
+        self.params = {'image_id': coco.getQuesCapIds()}
+        self.params1 = coco.getQuesCapIds()
+
+    def evaluate(self):
+        imgIds = self.params['image_id']
+        # imgIds1 = self.params1
+        # print "type of imgids ",type(imgIds)
+        # # print "imgids",imgIds
+        # # print "imgids1",imgIds1
+        # for x in xrange(1,10):
+        #     print "image ids in evaluate function",imgIds[x]
+        # imgIds = self.coco.getImgIds()
+        gts = {}
+        res = {}
+        # count =0
+        # print "image ids for gts COCO type ",type(self.coco.imgToAnns)
+        for imgId in imgIds:
+            # if count <= 10:
+            #     # for x in xrange(1,10):
+            #     # print "image ids for gts",self.coco.imgToAnns[imgId]
+            #     # print "image ids for gts len ",len(self.cocoRes.imgToAnns[imgId])
+            #     # gts[imgId] = self.coco.imgToAnns[imgId]
+            res[imgId] = self.cocoRes.imgToAnns[imgId]
+            gts[imgId] = self.coco.imgToAnns[imgId]
+                # print "image ids for res ",self.cocoRes.imgToAnns[imgId]
+                # print "image ids for res typr ",type(self.cocoRes.imgToAnns[imgId])
+                # count = count+1;
+
+        # for imgId in imgIds1:
+        #     if count <= 10:
+        #         # for x in xrange(1,10):
+        #         # print "image ids for gts",self.coco.imgToAnns[imgId]
+        #         # print "image ids for gts len ",len(self.cocoRes.imgToAnns[imgId])
+        #         gts[imgId] = self.coco.imgToAnns[imgId]
+        #         # res[imgId] = self.cocoRes.imgToAnns[imgId]
+        #         print "image ids for gts ",self.coco.imgToAnns[imgId]
+        #         print "image ids for gts typr ",type(self.coco.imgToAnns[imgId])
+        #         count = count+1;        
+               
+
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print 'tokenization...'
+        tokenizer = PTBTokenizer()
+        gts  = tokenizer.tokenize_gt(gts)
+        res = tokenizer.tokenize(res)
+
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print 'setting up scorers...'
+        scorers = [
+            (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
+            (Meteor(),"METEOR"),
+            (Rouge(), "ROUGE_L"),
+            (Cider(), "CIDEr")
+        ]
+
+        # =================================================
+        # Compute scores
+        # =================================================
+        for scorer, method in scorers:
+            print 'computing %s score...'%(scorer.method())
+            score, scores = scorer.compute_score(gts, res)
+            if type(method) == list:
+                for sc, scs, m in zip(score, scores, method):
+                    self.setEval(sc, m)
+                    self.setImgToEvalImgs(scs, gts.keys(), m)
+                    print "%s: %0.3f"%(m, sc)
+            else:
+                self.setEval(score, method)
+                self.setImgToEvalImgs(scores, gts.keys(), method)
+                print "%s: %0.3f"%(method, score)
+        self.setEvalImgs()
+
+    def setEval(self, score, method):
+        self.eval[method] = score
+
+    def setImgToEvalImgs(self, scores, imgIds, method):
+        for imgId, score in zip(imgIds, scores):
+            if not imgId in self.imgToEval:
+                self.imgToEval[imgId] = {}
+                self.imgToEval[imgId]["image_id"] = imgId
+            self.imgToEval[imgId][method] = score
+
+    def setEvalImgs(self):
+        self.evalImgs = [eval for imgId, eval in self.imgToEval.items()]
